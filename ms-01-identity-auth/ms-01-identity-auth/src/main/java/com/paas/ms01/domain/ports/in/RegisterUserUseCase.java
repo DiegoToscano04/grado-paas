3e09b5ca8e54125dcbc6b8413150c5a973e39df6
DiegoToscano04 <diego2201780@correo.uis.edu.co>
2026-02-17 16:11:22 -0500

    Interface Register

--- ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/domain/ports/in/RegisterUserUseCase.java
+++ ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/domain/ports/in/RegisterUserUseCase.java
@@ -1,4 +1,8 @@
 package com.paas.ms01.domain.ports.in;
 
+import com.paas.ms01.domain.model.RegisterUserCommand;
+import com.paas.ms01.infrastructure.adapter.out.persistence.UserEntity;
+
 public interface RegisterUserUseCase {
+    UserEntity register(RegisterUserCommand command);
 }
