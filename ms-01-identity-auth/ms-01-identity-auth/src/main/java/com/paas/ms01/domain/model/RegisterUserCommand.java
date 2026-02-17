8dbdef22112552dce566220195fcfec0538b7dab
DiegoToscano04 <diego2201780@correo.uis.edu.co>
2026-02-17 16:08:39 -0500

    DTO RegisterUserCommand

--- ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/domain/model/RegisterUserCommand.java
+++ ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/domain/model/RegisterUserCommand.java
@@ -1,4 +1,13 @@
 package com.paas.ms01.domain.model;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class RegisterUserCommand {
+    private String name;
+    private String email;
+    private String studentCode;
+    private String password;
 }
