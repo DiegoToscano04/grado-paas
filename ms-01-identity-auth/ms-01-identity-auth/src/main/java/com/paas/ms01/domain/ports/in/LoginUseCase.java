9c4fcc60a8eb1232018df112c4682dc20e02e19f
DiegoToscano04 <diego2201780@correo.uis.edu.co>
2026-02-17 16:10:52 -0500

    Interface Login

--- ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/domain/ports/in/LoginUseCase.java
+++ ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/domain/ports/in/LoginUseCase.java
@@ -1,4 +1,7 @@
 package com.paas.ms01.domain.ports.in;
+import com.paas.ms01.domain.model.LoginCommand;
+import com.paas.ms01.infrastructure.adapter.out.persistence.UserEntity;
 
 public interface LoginUseCase {
+    UserEntity login(LoginCommand command);
 }
