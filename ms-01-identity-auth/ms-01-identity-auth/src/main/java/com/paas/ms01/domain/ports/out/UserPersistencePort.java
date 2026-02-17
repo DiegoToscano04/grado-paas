f3dfd36b7662657255fb86f279f2d68e2ec73316
DiegoToscano04 <diego2201780@correo.uis.edu.co>
2026-02-17 16:14:26 -0500

    DATZ-Feat: Interface UserPersistencePort

--- ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/domain/ports/out/UserPersistencePort.java
+++ ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/domain/ports/out/UserPersistencePort.java
@@ -1,4 +1,11 @@
 package com.paas.ms01.domain.ports.out;
 
+import com.paas.ms01.infrastructure.adapter.out.persistence.UserEntity;
+import java.util.Optional;
+
 public interface UserPersistencePort {
+    UserEntity save(UserEntity user);
+    boolean existsByEmail(String email);
+    boolean existsByCode(String code);
+    Optional<UserEntity> findByEmail(String email);
 }
