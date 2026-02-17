9f89df10bb992387e375c5bb1b206e977dc7277d
DiegoToscano04 <diego2201780@correo.uis.edu.co>
2026-02-17 16:29:33 -0500

    DATZ-Feat: Load user from database

--- ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/infrastructure/config/UserDetailsServiceImpl.java
+++ ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/infrastructure/config/UserDetailsServiceImpl.java
@@ -1,4 +1,38 @@
 package com.paas.ms01.infrastructure.config;
 
-public class UserDetailsServiceImpl {
+import com.paas.ms01.domain.model.UserRole;
+import com.paas.ms01.infrastructure.adapter.out.persistence.UserEntity;
+import com.paas.ms01.infrastructure.adapter.out.persistence.UserJpaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserJpaRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        UserEntity userEntity = userRepository.findAll().stream()
+                .filter(u -> u.getEmail().equals(email)) // Aquí podrías optimizar con un findByEmail real en el Repo
+                .findFirst()
+                .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado"));
+
+        // Convertimos nuestra UserEntity al UserDetails interno de Spring Security
+        return new User(
+                userEntity.getEmail(),
+                userEntity.getPasswordHash(),
+                userEntity.getIsActive(),
+                true, true, true,
+                Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + userEntity.getRole().name()))
+        );
+    }
 }
