7cf724993b48664aa9c50d41f54195794aeea7e0
DiegoToscano04 <diego2201780@correo.uis.edu.co>
2026-02-18 09:55:17 -0500

    DATZ-Feat: Save projects in the database

--- ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/domain/ports/out/ProjectPersistencePort.java
+++ ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/domain/ports/out/ProjectPersistencePort.java
@@ -1,4 +1,13 @@
 package com.paas.ms01.domain.ports.out;
 
+import com.paas.ms01.infrastructure.adapter.out.persistence.ProjectEntity;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 public interface ProjectPersistencePort {
+    ProjectEntity save(ProjectEntity project);
+    Optional<ProjectEntity> findById(UUID id);
+    List<ProjectEntity> findByUserId(UUID userId);
+    boolean existsByNameAndUserId(String name, UUID userId);
 }
