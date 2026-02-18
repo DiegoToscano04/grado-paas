5d2fb0723817466f0431ecf5d82d9d90347d6e61
DiegoToscano04 <diego2201780@correo.uis.edu.co>
2026-02-18 10:00:55 -0500

    DATZ-Feat: ProjectPersistenceAdapter

--- ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/infrastructure/adapter/out/persistence/ProjectPersistenceAdapter.java
+++ ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/infrastructure/adapter/out/persistence/ProjectPersistenceAdapter.java
@@ -1,4 +1,36 @@
 package com.paas.ms01.infrastructure.adapter.out.persistence;
 
-public class ProjectPersistenceAdapter {
+import com.paas.ms01.domain.ports.out.ProjectPersistencePort;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
+public class ProjectPersistenceAdapter implements ProjectPersistencePort {
+
+    private final ProjectJpaRepository jpaRepository;
+
+    @Override
+    public ProjectEntity save(ProjectEntity project) {
+        return jpaRepository.save(project);
+    }
+
+    @Override
+    public Optional<ProjectEntity> findById(UUID id) {
+        return jpaRepository.findByIdAndDeletedAtIsNull(id);
+    }
+
+    @Override
+    public List<ProjectEntity> findByUserId(UUID userId) {
+        return jpaRepository.findByUserIdAndDeletedAtIsNull(userId);
+    }
+
+    @Override
+    public boolean existsByNameAndUserId(String name, UUID userId) {
+        return jpaRepository.existsByNameAndUserIdAndDeletedAtIsNull(name, userId);
+    }
 }
