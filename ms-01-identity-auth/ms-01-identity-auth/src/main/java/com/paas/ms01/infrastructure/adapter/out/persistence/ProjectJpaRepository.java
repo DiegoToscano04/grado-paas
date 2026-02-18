3bd5e7fb84f4e86625d5d7aefa42df5098e0b7f8
DiegoToscano04 <diego2201780@correo.uis.edu.co>
2026-02-18 09:58:52 -0500

    DATZ-Feat: ProjectJpaRepository

--- ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/infrastructure/adapter/out/persistence/ProjectJpaRepository.java
+++ ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/infrastructure/adapter/out/persistence/ProjectJpaRepository.java
@@ -1,4 +1,23 @@
 package com.paas.ms01.infrastructure.adapter.out.persistence;
 
-public interface ProjectJpaRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface ProjectJpaRepository extends JpaRepository<ProjectEntity, UUID> {
+
+    // Listar proyectos activos de un usuario
+    List<ProjectEntity> findByUserIdAndDeletedAtIsNull(UUID userId);
+
+    // Verificar si ya existe un proyecto con ese nombre para ese usuario (activo)
+    boolean existsByNameAndUserIdAndDeletedAtIsNull(String name, UUID userId);
+
+    // Buscar un proyecto específico por ID asegurando que no esté borrado
+    Optional<ProjectEntity> findByIdAndDeletedAtIsNull(UUID id);
 }
