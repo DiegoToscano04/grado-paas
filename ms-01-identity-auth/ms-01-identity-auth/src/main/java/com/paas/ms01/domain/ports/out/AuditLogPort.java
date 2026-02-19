a72ed7041ed45f8c768f75219b20c658ad085177
DiegoToscano04 <diego2201780@correo.uis.edu.co>
2026-02-19 16:24:25 -0500

    DATZ:Feat: Interface AufitLogPort

--- ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/domain/ports/out/AuditLogPort.java
+++ ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/domain/ports/out/AuditLogPort.java
@@ -1,4 +1,6 @@
 package com.paas.ms01.domain.ports.out;
+import com.paas.ms01.infrastructure.adapter.out.persistence.ProjectAuditLogEntity;
 
 public interface AuditLogPort {
+    void saveProjectAudit(ProjectAuditLogEntity auditLog);
 }
