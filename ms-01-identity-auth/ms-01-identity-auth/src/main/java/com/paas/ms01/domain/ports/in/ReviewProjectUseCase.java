ca245bf9aeb9c1d5507674d2dc10b76e2f51f935
DiegoToscano04 <diego2201780@correo.uis.edu.co>
2026-02-19 16:13:46 -0500

    DATZ:Feat: Interface ReviewProjectUseCase

--- ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/domain/ports/in/ReviewProjectUseCase.java
+++ ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/domain/ports/in/ReviewProjectUseCase.java
@@ -1,4 +1,7 @@
 package com.paas.ms01.domain.ports.in;
+import java.util.UUID;
 
 public interface ReviewProjectUseCase {
+    void approveProject(UUID projectId, UUID adminId);
+    void rejectProject(UUID projectId, UUID adminId, String reason);
 }
