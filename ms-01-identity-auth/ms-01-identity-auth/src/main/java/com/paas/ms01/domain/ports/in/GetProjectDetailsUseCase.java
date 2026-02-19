bb8410bcbcde36ae14ac0428ca09016bed038b55
DiegoToscano04 <diego2201780@correo.uis.edu.co>
2026-02-19 16:04:58 -0500

    Interface GetProjectDetailsUseCase

--- ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/domain/ports/in/GetProjectDetailsUseCase.java
+++ ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/domain/ports/in/GetProjectDetailsUseCase.java
@@ -1,4 +1,9 @@
 package com.paas.ms01.domain.ports.in;
 
+import com.paas.ms01.infrastructure.adapter.out.persistence.ProjectEntity;
+import java.util.Optional;
+import java.util.UUID;
+
 public interface GetProjectDetailsUseCase {
+    Optional<ProjectEntity> findByIdAndUserId(UUID projectId, UUID userId);
 }
