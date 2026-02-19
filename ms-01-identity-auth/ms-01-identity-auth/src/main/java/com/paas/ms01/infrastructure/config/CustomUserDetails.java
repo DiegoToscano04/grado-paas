f63b93cfa0de613958bbab95e97fa8da7012f9e8
DiegoToscano04 <diego2201780@correo.uis.edu.co>
2026-02-19 15:59:30 -0500

    DATZ-Feat: Inject UserEntity

--- ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/infrastructure/config/CustomUserDetails.java
+++ ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/infrastructure/config/CustomUserDetails.java
@@ -1,4 +1,17 @@
 package com.paas.ms01.infrastructure.config;
 
-public class CustomUserDetails {
+import com.paas.ms01.infrastructure.adapter.out.persistence.UserEntity;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import java.util.Collection;
+
+@Getter
+public class CustomUserDetails extends User {
+    private final UserEntity userEntity;
+
+    public CustomUserDetails(UserEntity userEntity, Collection<? extends GrantedAuthority> authorities) {
+        super(userEntity.getEmail(), userEntity.getPasswordHash(), authorities);
+        this.userEntity = userEntity;
+    }
 }
