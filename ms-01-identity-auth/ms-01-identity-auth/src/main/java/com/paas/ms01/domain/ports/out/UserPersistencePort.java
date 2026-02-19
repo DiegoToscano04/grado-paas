088bff228754e48a227b1ddab2267b003a9e5171
DiegoToscano04 <diego2201780@correo.uis.edu.co>
2026-02-19 15:55:49 -0500

    DATZ-Feat: Add findById

--- ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/domain/ports/out/UserPersistencePort.java
+++ ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/domain/ports/out/UserPersistencePort.java
@@ -2,10 +2,12 @@ package com.paas.ms01.domain.ports.out;
 
 import com.paas.ms01.infrastructure.adapter.out.persistence.UserEntity;
 import java.util.Optional;
+import java.util.UUID;
 
 public interface UserPersistencePort {
     UserEntity save(UserEntity user);
     boolean existsByEmail(String email);
     boolean existsByCode(String code);
     Optional<UserEntity> findByEmail(String email);
+    Optional<UserEntity> findById(UUID userId);
 }
