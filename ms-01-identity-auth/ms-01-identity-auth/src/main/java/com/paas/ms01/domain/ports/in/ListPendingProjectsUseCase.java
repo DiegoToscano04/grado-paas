f1fa2f112e873a021bfa5e3700ab65adf0357c0b
DiegoToscano04 <diego2201780@correo.uis.edu.co>
2026-02-19 16:12:32 -0500

    DATZ:Feat: Interface ListPendingProjectUseCase

--- ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/domain/ports/in/ListPendingProjectsUseCase.java
+++ ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/domain/ports/in/ListPendingProjectsUseCase.java
@@ -1,4 +1,7 @@
 package com.paas.ms01.domain.ports.in;
+import com.paas.ms01.infrastructure.adapter.out.persistence.ProjectEntity;
+import java.util.List;
 
-public interface ListPendingProjectUseCase {
+public interface ListPendingProjectsUseCase {
+    List<ProjectEntity> getPendingApprovalProjects();
 }
