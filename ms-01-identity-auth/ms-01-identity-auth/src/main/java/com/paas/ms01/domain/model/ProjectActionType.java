b5b06ec9e011f89b0d621ee268ef98e268169478
DiegoToscano04 <diego2201780@correo.uis.edu.co>
2026-02-19 16:18:55 -0500

    DATZ:Feat: Enum ProjectActionType

--- ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/domain/model/ProjectActionType.java
+++ ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/domain/model/ProjectActionType.java
@@ -1,4 +1,16 @@
 package com.paas.ms01.domain.model;
 
 public enum ProjectActionType {
+    CREATE,
+    APPROVE,
+    REJECT,
+    DEPLOY_START,
+    DEPLOY_SUCCESS,
+    DEPLOY_FAILED,
+    REDEPLOY,
+    REDEPLOY_START,
+    REDEPLOY_SUCCESS,
+    REDEPLOY_FAILED,
+    TERMINATE,
+    DELETE
 }
