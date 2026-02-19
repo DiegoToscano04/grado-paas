026c6698a427b52a602e82117085e4378127100b
DiegoToscano04 <diego2201780@correo.uis.edu.co>
2026-02-19 17:00:23 -0500

    DATZ-Feat: Interface ManageUserQuotasUseCase

--- ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/domain/ports/in/ManageUserQuotasUseCase.java
+++ ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/domain/ports/in/ManageUserQuotasUseCase.java
@@ -1,4 +1,8 @@
 package com.paas.ms01.domain.ports.in;
+import com.paas.ms01.infrastructure.adapter.out.persistence.UserEntity;
+import java.math.BigDecimal;
+import java.util.UUID;
 
 public interface ManageUserQuotasUseCase {
+    UserEntity updateUserQuotas(UUID userId, BigDecimal cpu, Integer memoryMb, Integer storageMb, UUID adminId);
 }
