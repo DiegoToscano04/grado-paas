c82440e178dfff214d99bd73a768524d873c2d2a
DiegoToscano04 <diego2201780@correo.uis.edu.co>
2026-02-19 16:21:50 -0500

    DATZ:Feat: Map table project_audit_logs

--- ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/infrastructure/adapter/out/persistence/ProjectAuditLogEntity.java
+++ ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/infrastructure/adapter/out/persistence/ProjectAuditLogEntity.java
@@ -1,4 +1,52 @@
 package com.paas.ms01.infrastructure.adapter.out.persistence;
 
+import com.paas.ms01.domain.model.ProjectActionType;
+import com.paas.ms01.domain.model.ProjectStatus;
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "project_audit_logs", schema = "paas_core")
+@Data
 public class ProjectAuditLogEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "project_id", nullable = false)
+    private UUID projectId;
+
+    @Enumerated(EnumType.STRING)
+    @JdbcTypeCode(SqlTypes.NAMED_ENUM)
+    @Column(name = "previous_status", columnDefinition = "paas_core.project_status")
+    private ProjectStatus previousStatus;
+
+    @Enumerated(EnumType.STRING)
+    @JdbcTypeCode(SqlTypes.NAMED_ENUM)
+    @Column(name = "new_status", columnDefinition = "paas_core.project_status")
+    private ProjectStatus newStatus;
+
+    @Column(name = "changed_by_user_id")
+    private UUID changedByUserId;
+
+    @Enumerated(EnumType.STRING)
+    @JdbcTypeCode(SqlTypes.NAMED_ENUM)
+    @Column(columnDefinition = "paas_core.project_action_type", nullable = false)
+    private ProjectActionType action;
+
+    private String reason;
+
+    @JdbcTypeCode(SqlTypes.JSON)
+    private String details;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
 }
