21a0d7447ced435dddd7e187553d56ba03d766e3
DiegoToscano04 <diego2201780@correo.uis.edu.co>
2026-02-19 16:29:03 -0500

    DATZ:Feat: ProjectPersistencePort

--- ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/domain/ports/out/ProjectPersistencePort.java
+++ ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/domain/ports/out/ProjectPersistencePort.java
@@ -1,5 +1,6 @@
 package com.paas.ms01.domain.ports.out;
 
+import com.paas.ms01.domain.model.ProjectStatus;
 import com.paas.ms01.infrastructure.adapter.out.persistence.ProjectEntity;
 import java.util.List;
 import java.util.Optional;
@@ -10,4 +11,6 @@ public interface ProjectPersistencePort {
     Optional<ProjectEntity> findById(UUID id);
     List<ProjectEntity> findByUserId(UUID userId);
     boolean existsByNameAndUserId(String name, UUID userId);
+    List<ProjectEntity> findByStatus(ProjectStatus status);
+
 }
