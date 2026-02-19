ef008fd56e910bad228fd44bbd80787fdc6bdfb7
DiegoToscano04 <diego2201780@correo.uis.edu.co>
2026-02-19 16:22:40 -0500

    DATZ:Feat: ProjectAuditLogJpaRepository

--- ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/infrastructure/adapter/out/persistence/ProjectAuditLogJpaRepository.java
+++ ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/infrastructure/adapter/out/persistence/ProjectAuditLogJpaRepository.java
@@ -1,4 +1,8 @@
 package com.paas.ms01.infrastructure.adapter.out.persistence;
 
-public interface ProjectAuditLogJpaRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectAuditLogJpaRepository extends JpaRepository<ProjectAuditLogEntity, Long> {
 }
