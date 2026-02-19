04efe4b6387df9785a83730651ac9aa7af802658
DiegoToscano04 <diego2201780@correo.uis.edu.co>
2026-02-19 16:59:40 -0500

    DATZ-Feat: Interface ListUserUserCase

--- ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/domain/ports/in/ListUsersUseCase.java
+++ ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/domain/ports/in/ListUsersUseCase.java
@@ -1,4 +1,7 @@
 package com.paas.ms01.domain.ports.in;
+import com.paas.ms01.infrastructure.adapter.out.persistence.UserEntity;
+import java.util.List;
 
 public interface ListUsersUseCase {
+    List<UserEntity> getAllStudents();
 }
