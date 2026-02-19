83de088678d9c035bee251be1ec34ad649bc4577
DiegoToscano04 <diego2201780@correo.uis.edu.co>
2026-02-19 15:50:27 -0500

    DATZ-Feat: Define action

--- ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/domain/ports/in/CreateProjectUseCase.java
+++ ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/domain/ports/in/CreateProjectUseCase.java
@@ -1,4 +1,8 @@
 package com.paas.ms01.domain.ports.in;
 
+import com.paas.ms01.domain.model.CreateProjectCommand;
+import com.paas.ms01.infrastructure.adapter.out.persistence.ProjectEntity;
+
 public interface CreateProjectUseCase {
+    ProjectEntity createProject(CreateProjectCommand command);
 }
