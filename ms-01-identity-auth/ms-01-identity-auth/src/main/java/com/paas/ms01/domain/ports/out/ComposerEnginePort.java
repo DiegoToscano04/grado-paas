f9ec527164bd81ca531b54985ece70b1d75a70db
DiegoToscano04 <diego2201780@correo.uis.edu.co>
2026-02-25 16:22:58 -0500

    DATZ-Feat: updating interface ComposerEnginePort

--- ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/domain/ports/out/ComposerEnginePort.java
+++ ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/domain/ports/out/ComposerEnginePort.java
@@ -4,5 +4,4 @@ import com.paas.ms01.domain.model.AppArchitecture;
 import com.paas.ms01.domain.model.ValidationResult;
 
 public interface ComposerEnginePort {
-    ValidationResult validateCompose(AppArchitecture architecture, String composeContent);
-}
+    ValidationResult validateCompose(AppArchitecture architecture, String composeContent, String namespaceName);}
