0941453230add9af88d765a6fdade9bb14896efc
DiegoToscano04 <diego2201780@correo.uis.edu.co>
2026-02-25 15:18:02 -0500

    DATZ-Feat: Record ValidationResult

--- ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/domain/model/ValidationResult.java
+++ ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/domain/model/ValidationResult.java
@@ -1,4 +1,13 @@
 package com.paas.ms01.domain.model;
 
-public record ValidationResult() {
-}
+import java.math.BigDecimal;
+import java.util.List;
+
+public record ValidationResult(
+        boolean isValid,
+        List<String> errors,
+        BigDecimal requiredCpu,
+        int requiredMemoryMb,
+        int requiredStorageMb,
+        List<String> manifests
+) {}
