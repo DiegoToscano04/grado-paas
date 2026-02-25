d1e6897f519b5b4e094206de87d7896f66249217
DiegoToscano04 <diego2201780@correo.uis.edu.co>
2026-02-25 11:45:53 -0500

    DATZ-Feat: Add new method

--- ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/domain/ports/out/AuditLogPort.java
+++ ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/domain/ports/out/AuditLogPort.java
@@ -1,6 +1,8 @@
 package com.paas.ms01.domain.ports.out;
 import com.paas.ms01.infrastructure.adapter.out.persistence.ProjectAuditLogEntity;
+import com.paas.ms01.infrastructure.adapter.out.persistence.UserAuditLogEntity;
 
 public interface AuditLogPort {
     void saveProjectAudit(ProjectAuditLogEntity auditLog);
+    void saveUserAudit(UserAuditLogEntity auditLog);
 }
