a4633221c786e828e90fa51d9d3ac2aaa396eb2e
DiegoToscano04 <diego2201780@correo.uis.edu.co>
2026-02-25 16:27:55 -0500

    DATZ-Feat: updating schemas to receive namespace

--- ms-02-composer-engine/app/models/schemas.py
+++ ms-02-composer-engine/app/models/schemas.py
@@ -2,6 +2,7 @@ from pydantic import BaseModel, Field
 from enum import Enum
 from typing import List, Optional
 
+
 # Debe coincidir exactamente con el Enum de Java (MS-01)
 class AppArchitecture(str, Enum):
     DB_STANDALONE = "DB_STANDALONE"
@@ -12,15 +13,20 @@ class AppArchitecture(str, Enum):
     MONOLITH_DB = "MONOLITH_DB"
     THREE_TIER = "THREE_TIER"
 
+
 class ComposeValidationRequest(BaseModel):
     architecture: AppArchitecture
-    compose_content: str = Field(..., description="Contenido en texto del archivo docker-compose.yaml")
+    compose_content: str = Field(
+        ..., description="Contenido en texto del archivo docker-compose.yaml"
+    )
+    namespace_name: str = Field(..., description="Namespace único del proyecto")
+
 
 class ComposeValidationResponse(BaseModel):
     is_valid: bool
-    errors: List[str] =[]
+    errors: List[str] = []
     # Estos valores los calcularemos leyendo el YAML
     required_cpu: float = 0.0
     required_memory_mb: int = 0
     required_storage_mb: int = 0
-    manifests: List[str] =[] # Por ahora vacío, luego pondremos los generados
+    manifests: List[str] = []  # Por ahora vacío, luego pondremos los generados
