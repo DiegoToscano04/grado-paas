ae7e0b2ef598614ea22d505faa469caf9e830bec
DiegoToscano04 <diego2201780@correo.uis.edu.co>
2026-02-26 18:07:28 -0500

    DATZ-Feat: create rabbitmq_config.py to connect with RAbbitMQ

---
+++ ms-03-k8s-orchestrator/app/core/rabbitmq_config.py
@@ -0,0 +1,17 @@
+import pika
+import os
+
+RABBITMQ_HOST = os.getenv("RABBITMQ_HOST", "localhost")
+RABBITMQ_USER = os.getenv("RABBITMQ_USER", "admin")
+RABBITMQ_PASS = os.getenv("RABBITMQ_PASS", "secret")
+
+
+def get_rabbitmq_connection():
+    credentials = pika.PlainCredentials(RABBITMQ_USER, RABBITMQ_PASS)
+    parameters = pika.ConnectionParameters(
+        host=RABBITMQ_HOST,
+        credentials=credentials,
+        heartbeat=600,
+        blocked_connection_timeout=300,
+    )
+    return pika.BlockingConnection(parameters)
