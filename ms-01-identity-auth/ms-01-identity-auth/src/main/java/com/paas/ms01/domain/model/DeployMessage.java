5b334a252bc252c9747a956d349137e7bc808efe
DiegoToscano04 <diego2201780@correo.uis.edu.co>
2026-02-26 19:08:08 -0500

    DATZ-Feat: Record DeployMessage

--- ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/domain/model/DeployMessage.java
+++ ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/domain/model/DeployMessage.java
@@ -1,4 +1,10 @@
 package com.paas.ms01.domain.model;
 
-public record DeployMessage() {
-}
+import java.util.List;
+import java.util.UUID;
+
+public record DeployMessage(
+        UUID projectId,
+        String namespaceName,
+        List<String> manifests
+) {}
