7f70ee0148797bdcd26e214d8424416832ddb11a
DiegoToscano04 <diego2201780@correo.uis.edu.co>
2026-02-26 19:09:25 -0500

    DATZ-Feat: RabbitMqAdapter

--- ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/infrastructure/adapter/out/messaging/RabbitMqAdapter.java
+++ ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/infrastructure/adapter/out/messaging/RabbitMqAdapter.java
@@ -1,4 +1,45 @@
 package com.paas.ms01.infrastructure.adapter.out.messaging;
 
-public class RabbitMqAdapter {
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.paas.ms01.domain.model.DeployMessage;
+import com.paas.ms01.domain.ports.out.DeployMessagePort;
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class RabbitMqAdapter implements DeployMessagePort {
+
+    private final RabbitTemplate rabbitTemplate;
+    private final ObjectMapper objectMapper;
+    private static final String QUEUE_NAME = "deploy.queue";
+
+    @Override
+    public void sendDeployCommand(DeployMessage message) {
+        try {
+            // Convertimos nuestro Record a JSON
+            String jsonMessage = objectMapper.writeValueAsString(message);
+
+            // Enviamos el mensaje a la cola
+            rabbitTemplate.convertAndSend(QUEUE_NAME, jsonMessage);
+
+            System.out.println("✅ [MS-01] Mensaje enviado a RabbitMQ para el proyecto: " + message.projectId());
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Error al serializar el mensaje de despliegue", e);
+        }
+    }
+
+    // --- Configuración interna para asegurar que la cola exista en Java también ---
+    @Configuration
+    static class RabbitConfig {
+        @Bean
+        public Queue deployQueue() {
+            return new Queue(QUEUE_NAME, true); // true = durable
+        }
+    }
 }
