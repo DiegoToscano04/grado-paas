65530113f4dad521ad764ca2aea814b7f4d95288
DiegoToscano04 <diego2201780@correo.uis.edu.co>
2026-02-26 19:08:45 -0500

    DATZ-Feat: Interface DeployMessagePort

--- ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/domain/ports/out/DeployMessagePort.java
+++ ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/domain/ports/out/DeployMessagePort.java
@@ -1,4 +1,7 @@
 package com.paas.ms01.domain.ports.out;
 
+import com.paas.ms01.domain.model.DeployMessage;
+
 public interface DeployMessagePort {
+    void sendDeployCommand(DeployMessage message);
 }
