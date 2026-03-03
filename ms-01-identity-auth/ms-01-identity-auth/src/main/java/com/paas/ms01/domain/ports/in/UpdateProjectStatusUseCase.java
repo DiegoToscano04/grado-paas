38f302969ae219101e89b7b27066b96807d55b11
DiegoToscano04 <diego2201780@correo.uis.edu.co>
2026-03-02 19:49:38 -0500

    DATZ-Feat: New interface UpdateProjectStatusUseCase

--- ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/domain/ports/in/UpdateProjectStatusUseCase.java
+++ ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/domain/ports/in/UpdateProjectStatusUseCase.java
@@ -1,4 +1,8 @@
 package com.paas.ms01.domain.ports.in;
 
+import com.paas.ms01.domain.model.ProjectStatus;
+import java.util.UUID;
+
 public interface UpdateProjectStatusUseCase {
+    void updateStatus(UUID projectId, ProjectStatus newStatus, String message);
 }
