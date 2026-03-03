271a5fbfd7dac8046c3daa3112c6728d317debae
DiegoToscano04 <diego2201780@correo.uis.edu.co>
2026-03-02 19:56:56 -0500

    DATZ-Feat: New queue termiante.queue and new method sendTerminateCommand

--- ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/infrastructure/adapter/out/messaging/RabbitMqAdapter.java
+++ ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/infrastructure/adapter/out/messaging/RabbitMqAdapter.java
@@ -3,7 +3,9 @@ package com.paas.ms01.infrastructure.adapter.out.messaging;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.paas.ms01.domain.model.DeployMessage;
+import com.paas.ms01.domain.model.TerminateMessage;
 import com.paas.ms01.domain.ports.out.DeployMessagePort;
+import com.paas.ms01.domain.ports.out.TerminateMessagePort;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -13,11 +15,12 @@ import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-public class RabbitMqAdapter implements DeployMessagePort {
+public class RabbitMqAdapter implements DeployMessagePort, TerminateMessagePort {
 
     private final RabbitTemplate rabbitTemplate;
     private final ObjectMapper objectMapper;
-    private static final String QUEUE_NAME = "deploy.queue";
+    private static final String DEPLOY_QUEUE = "deploy.queue";
+    private static final String TERMINATE_QUEUE = "terminate.queue";
 
     @Override
     public void sendDeployCommand(DeployMessage message) {
@@ -26,11 +29,22 @@ public class RabbitMqAdapter implements DeployMessagePort {
             String jsonMessage = objectMapper.writeValueAsString(message);
 
             // Enviamos el mensaje a la cola
-            rabbitTemplate.convertAndSend(QUEUE_NAME, jsonMessage);
+            rabbitTemplate.convertAndSend(DEPLOY_QUEUE, jsonMessage);
 
             System.out.println("✅ [MS-01] Mensaje enviado a RabbitMQ para el proyecto: " + message.projectId());
         } catch (JsonProcessingException e) {
             throw new RuntimeException("Error al serializar el mensaje de despliegue", e);
+        }
+    }
+
+    @Override
+    public void sendTerminateCommand(TerminateMessage message) {
+        try {
+            String jsonMessage = objectMapper.writeValueAsString(message);
+            rabbitTemplate.convertAndSend(TERMINATE_QUEUE, jsonMessage);
+            System.out.println("🧨 [MS-01] Mensaje de DESTRUCCIÓN enviado para el proyecto: " + message.projectId());
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Error al serializar el mensaje de terminación", e);
         }
     }
 
@@ -39,7 +53,12 @@ public class RabbitMqAdapter implements DeployMessagePort {
     static class RabbitConfig {
         @Bean
         public Queue deployQueue() {
-            return new Queue(QUEUE_NAME, true); // true = durable
+            return new Queue(DEPLOY_QUEUE, true);
+        }
+
+        @Bean
+        public Queue terminateQueue() { // <--- REGISTRAR LA NUEVA COLA
+            return new Queue(TERMINATE_QUEUE, true);
         }
     }
 }
