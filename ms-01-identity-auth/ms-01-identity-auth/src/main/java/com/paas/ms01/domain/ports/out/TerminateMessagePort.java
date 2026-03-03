309f1884635dc0a16dfae6e6e1574487816967d7
DiegoToscano04 <diego2201780@correo.uis.edu.co>
2026-03-02 19:55:31 -0500

    DATZ-Feat: New interface TerminateMessagePort

--- ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/domain/ports/out/TerminateMessagePort.java
+++ ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/domain/ports/out/TerminateMessagePort.java
@@ -1,4 +1,7 @@
 package com.paas.ms01.domain.ports.out;
 
+import com.paas.ms01.domain.model.TerminateMessage;
+
 public interface TerminateMessagePort {
+    void sendTerminateCommand(TerminateMessage message);
 }
