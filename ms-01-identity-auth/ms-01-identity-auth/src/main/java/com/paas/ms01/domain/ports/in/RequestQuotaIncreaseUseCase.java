fa463854cf4419bc1e16976f2f67bf741f7d3913
DiegoToscano04 <diego2201780@correo.uis.edu.co>
2026-03-04 17:20:36 -0500

    DATZ-Feat: Port in RequestQuotaIncreaseUseCase

--- ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/domain/ports/in/RequestQuotaIncreaseUseCase.java
+++ ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/domain/ports/in/RequestQuotaIncreaseUseCase.java
@@ -1,4 +1,8 @@
 package com.paas.ms01.domain.ports.in;
 
+import com.paas.ms01.domain.model.CreateQuotaRequestCommand;
+import com.paas.ms01.infrastructure.adapter.out.persistence.QuotaRequestEntity;
+
 public interface RequestQuotaIncreaseUseCase {
+    QuotaRequestEntity requestIncrease(CreateQuotaRequestCommand command);
 }
