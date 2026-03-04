96b7ccbd9f370d973558363772a935056b2a6ed9
DiegoToscano04 <diego2201780@correo.uis.edu.co>
2026-03-04 17:29:50 -0500

    DATZ-Feat: Interface ReviewQuotaRequestUseCase

--- ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/domain/ports/in/ReviewQuotaRequestUseCase.java
+++ ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/domain/ports/in/ReviewQuotaRequestUseCase.java
@@ -1,4 +1,6 @@
 package com.paas.ms01.domain.ports.in;
+import java.util.UUID;
 
 public interface ReviewQuotaRequestUseCase {
+    void reviewRequest(Long requestId, boolean approve, String adminResponse, UUID adminId);
 }
