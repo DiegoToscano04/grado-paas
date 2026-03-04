e72476eb22f86c0a9c5a216f7264caae4ab6c5ea
DiegoToscano04 <diego2201780@correo.uis.edu.co>
2026-03-04 17:15:03 -0500

    DATZ-Feat: Port Interface QuotaRequestPersistencePort

--- ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/domain/ports/out/QuotaRequestPersistencePort.java
+++ ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/domain/ports/out/QuotaRequestPersistencePort.java
@@ -1,4 +1,15 @@
 package com.paas.ms01.domain.ports.out;
 
+import com.paas.ms01.domain.model.QuotaStatus;
+import com.paas.ms01.infrastructure.adapter.out.persistence.QuotaRequestEntity;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 public interface QuotaRequestPersistencePort {
+    QuotaRequestEntity save(QuotaRequestEntity request);
+    boolean hasPendingRequest(UUID userId);
+    List<QuotaRequestEntity> findByStatus(QuotaStatus status);
+    Optional<QuotaRequestEntity> findById(Long id);
 }
