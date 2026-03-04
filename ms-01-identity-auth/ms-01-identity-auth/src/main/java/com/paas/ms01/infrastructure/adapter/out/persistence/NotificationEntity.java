86c1f6f8fb54a87d61f661fcf2bba84841896f7d
DiegoToscano04 <diego2201780@correo.uis.edu.co>
2026-03-04 18:12:13 -0500

    DATZ-Feat: map new table notifications

---
+++ ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/infrastructure/adapter/out/persistence/NotificationEntity.java
@@ -0,0 +1,34 @@
+package com.paas.ms01.infrastructure.adapter.out.persistence;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "notifications", schema = "paas_core")
+@Data
+public class NotificationEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "user_id", nullable = false)
+    private UUID userId;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String message;
+
+    @Column(name = "is_read")
+    private Boolean isRead = false;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+}
