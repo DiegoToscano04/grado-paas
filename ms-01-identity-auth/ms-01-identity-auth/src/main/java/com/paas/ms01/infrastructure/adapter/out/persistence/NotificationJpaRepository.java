2d9ce2f38a80eb1fc7b8155dd59cecdf40c66ca2
DiegoToscano04 <diego2201780@correo.uis.edu.co>
2026-03-04 18:13:12 -0500

    DATZ-Feat: Interface NotificationJpaRepository

--- ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/infrastructure/adapter/out/persistence/NotificationJpaRepository.java
+++ ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/infrastructure/adapter/out/persistence/NotificationJpaRepository.java
@@ -1,4 +1,12 @@
 package com.paas.ms01.infrastructure.adapter.out.persistence;
 
-public interface NotificationJpaRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface NotificationJpaRepository extends JpaRepository<NotificationEntity, UUID> {
+    // Para traer las notificaciones más nuevas primero
+    List<NotificationEntity> findByUserIdOrderByCreatedAtDesc(UUID userId);
 }
