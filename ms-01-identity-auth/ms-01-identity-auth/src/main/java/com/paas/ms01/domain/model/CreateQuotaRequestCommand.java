9c32c99e911a19e994c5799f242d049992b8f478
DiegoToscano04 <diego2201780@correo.uis.edu.co>
2026-03-04 17:18:57 -0500

    DATZ-Feat: Command CreateQuotaRequestCommand

--- ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/domain/model/CreateQuotaRequestCommand.java
+++ ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/domain/model/CreateQuotaRequestCommand.java
@@ -1,4 +1,16 @@
 package com.paas.ms01.domain.model;
 
+import lombok.Builder;
+import lombok.Data;
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Data
+@Builder
 public class CreateQuotaRequestCommand {
+    private UUID userId;
+    private BigDecimal requestedCpu;
+    private Integer requestedMemoryMb;
+    private Integer requestedStorageMb;
+    private String justification;
 }
