5d501f8fb35b0b427082d43d09138977dc2c8311
DiegoToscano04 <diego2201780@correo.uis.edu.co>
2026-03-04 17:13:46 -0500

    DATZ-Feat: Interface QuotaRequestJpaRepository

--- ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/infrastructure/adapter/out/persistence/QuotaRequestJpaRepository.java
+++ ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/infrastructure/adapter/out/persistence/QuotaRequestJpaRepository.java
@@ -1,4 +1,15 @@
 package com.paas.ms01.infrastructure.adapter.out.persistence;
 
-public interface QuotaRequestJpaRepository {
+import com.paas.ms01.domain.model.QuotaStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface QuotaRequestJpaRepository extends JpaRepository<QuotaRequestEntity, Long> {
+    // Nos servirá para verificar si ya tiene una solicitud pendiente
+    boolean existsByUserIdAndStatus(UUID userId, QuotaStatus status);
+    List<QuotaRequestEntity> findByStatusOrderByCreatedAtAsc(QuotaStatus status);
 }
