b16264ba4abee963ee92448d0076db8ea9db0fa2
DiegoToscano04 <diego2201780@correo.uis.edu.co>
2026-03-04 17:28:16 -0500

    DATZ-Feat: Interface ListPendingQuotaRequestsUseCase

--- ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/domain/ports/in/ListPendingQuotaRequestsUseCase.java
+++ ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/domain/ports/in/ListPendingQuotaRequestsUseCase.java
@@ -1,4 +1,7 @@
 package com.paas.ms01.domain.ports.in;
+import com.paas.ms01.infrastructure.adapter.out.persistence.QuotaRequestEntity;
+import java.util.List;
 
 public interface ListPendingQuotaRequestsUseCase {
+    List<QuotaRequestEntity> getPendingRequests();
 }
