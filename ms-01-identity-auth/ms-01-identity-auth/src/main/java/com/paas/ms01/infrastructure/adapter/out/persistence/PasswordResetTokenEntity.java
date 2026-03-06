11b35d5c7abe7afc82a9e97c553e349b764fcf67
DiegoToscano04 <diego2201780@correo.uis.edu.co>
2026-03-06 14:58:18 -0500

    DATZ-Feat: map password_reset_tokens table

--- ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/infrastructure/adapter/out/persistence/PasswordResetTokenEntity.java
+++ ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/infrastructure/adapter/out/persistence/PasswordResetTokenEntity.java
@@ -1,4 +1,31 @@
 package com.paas.ms01.infrastructure.adapter.out.persistence;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "password_reset_tokens", schema = "paas_core")
+@Data
 public class PasswordResetTokenEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_id", nullable = false)
+    private UUID userId;
+
+    @Column(nullable = false, unique = true)
+    private String token;
+
+    @Column(name = "expires_at", nullable = false)
+    private LocalDateTime expiresAt;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
 }
