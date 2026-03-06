f23a391815d0fb786f92003c27c625e6081515d8
DiegoToscano04 <diego2201780@correo.uis.edu.co>
2026-03-06 14:59:42 -0500

    DATZ-Feat: Interface PasswordResetTokenJpaRepository

--- ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/infrastructure/adapter/out/persistence/PasswordResetTokenJpaRepository.java
+++ ms-01-identity-auth/ms-01-identity-auth/src/main/java/com/paas/ms01/infrastructure/adapter/out/persistence/PasswordResetTokenJpaRepository.java
@@ -1,4 +1,13 @@
 package com.paas.ms01.infrastructure.adapter.out.persistence;
 
-public interface PasswordResetTokenJpaRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface PasswordResetTokenJpaRepository extends JpaRepository<PasswordResetTokenEntity, Long> {
+    Optional<PasswordResetTokenEntity> findByToken(String token);
+    void deleteByUserId(UUID userId); // Útil para borrar tokens viejos cuando pidan uno nuevo
 }
